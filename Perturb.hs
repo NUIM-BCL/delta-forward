e61e5098d4c66e47090472642c0e349a4680b247
Barak A. Pearlmutter <barak+git@pearlmutter.net>
2015-11-26 20:19:37 +0000

    get lists working

--- Perturb.hs
+++ Perturb.hs
@@ -18,11 +18,11 @@ class Perturbable a where
 instance Perturbable Double where
   perturbs ε x = [(x+ε,const)]
 
--- instance Perturbable a ⇒ Perturbable [a] where
---   perturbs ε [] = []
---   perturbs ε (x:xs) = [] ++ [(x,XXX):ys |  ← perturbs ε xs]
-
--- map (:xs) (perturbs ε x) ++ map (x:) (perturbs ε xs)
+instance Perturbable a ⇒ Perturbable [a] where
+  perturbs ε [] = []
+  perturbs ε (x:xs) =
+    [(x':xs, \z (y:ys) → x_ z y : ys) | (x',x_) ← perturbs ε x] ++
+    [(x:xs',\z (y:ys) → y : xs_ z ys) | (xs',xs_) ← perturbs ε xs]
 
 instance (Perturbable a, Perturbable b) ⇒ Perturbable (a,b) where
   perturbs ε (x,y) =
@@ -45,3 +45,6 @@ grad ε f x = foldl step x $ perturbs ε x
 
 -- *Perturb> grad 1e-6 (\(x,(y,z)) -> x^2 + y^3 + z^5) (1,(2,3))
 -- (2.0000009897103155,(12.000005995105312,405.00027006373784))
+
+-- *Perturb> grad 1e-3 (sqrt.sum.map (^2)) [1..5]
+-- [0.13490616545741574,0.2697424594266806,0.40457630202705985,0.5394076933935565,0.6742366336585093]
