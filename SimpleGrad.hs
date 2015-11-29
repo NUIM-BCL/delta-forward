ff78d3618eb59d6c2de9a7f4ef4043afe9364664
Barak A. Pearlmutter <barak+git@pearlmutter.net>
2015-11-29 23:10:02 +0000

    switch to symmetric difference, and clean up Grad [a] code

--- SimpleGrad.hs
+++ SimpleGrad.hs
@@ -10,12 +10,16 @@ class Grad a where
   grad ∷ Double → (a → Double) → a → a
 
 instance Grad Double where
-  grad ε f x = (f (x+ε) - f x) / ε
+  -- Asymmetric Difference:
+  -- grad ε f x = (f (x+ε) - f x) / ε         
+  -- Symmetric Difference:
+  grad ε f x = (f (x+ε) - f (x-ε)) / (2⋅ε)
 
 instance Grad a ⇒ Grad [a] where
   grad ε f xs =
     [grad ε (f_ i) x_i | (x_i,i) ← zip xs [0..]] where
-        f_ i x_i = f $ take i xs ++ [x_i] ++ drop (i+1) xs
+      f_ i x_i = f $ xs1 ++ [x_i] ++ xs2 where
+        (xs1, (_:xs2)) = splitAt i xs
 
 instance (Grad a, Grad b) ⇒ Grad (a,b) where
   grad ε f (x,y) = (grad ε (f ∘ (,y)) x, grad ε (f ∘ (x,)) y)
