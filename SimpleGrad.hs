6639a75fdec6f5162f670c29f41948298280a638
Barak A. Pearlmutter <barak+git@pearlmutter.net>
2015-11-29 23:10:33 +0000

    further clean up Grad [a] code, make it symmetric to other algebraic datatypes

--- SimpleGrad.hs
+++ SimpleGrad.hs
@@ -16,10 +16,8 @@ instance Grad Double where
   grad ε f x = (f (x+ε) - f (x-ε)) / (2⋅ε)
 
 instance Grad a ⇒ Grad [a] where
-  grad ε f xs =
-    [grad ε (f_ i) x_i | (x_i,i) ← zip xs [0..]] where
-      f_ i x_i = f $ xs1 ++ [x_i] ++ xs2 where
-        (xs1, (_:xs2)) = splitAt i xs
+  grad ε f [] = []
+  grad ε f (x:xs) = grad ε (f ∘ (:xs)) x : grad ε (f ∘ (x:)) xs
 
 instance (Grad a, Grad b) ⇒ Grad (a,b) where
   grad ε f (x,y) = (grad ε (f ∘ (,y)) x, grad ε (f ∘ (x,)) y)
